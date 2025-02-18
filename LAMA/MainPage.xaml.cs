ee257c2b02c6b373a482209c302024aaff8bb24d
David James Street <62122942+dneutron0@users.noreply.github.com>
2025-02-18 14:55:39 -0600

    Add project files.

---
+++ LAMA/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace LAMA
+{
+    public partial class MainPage : ContentPage
+    {
+        public double ScreenWidth => DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density;
+        public double ScreenHeight => DeviceDisplay.MainDisplayInfo.Height / DeviceDisplay.MainDisplayInfo.Density;
+        
+        public double AppIconSize { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            AppIconSize = ScreenWidth * .3;
+
+
+        }
+
+      
+    }
+
+}
