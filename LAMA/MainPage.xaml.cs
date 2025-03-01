e6776e1746b011ed377f5a86e69e1c0595e4d920
Wgaines5 <willie.8chd@gmail.com>
2025-02-28 21:28:13 -0500

    Home Screen Created with bottom navigation connection to other pages

--- LAMA/MainPage.xaml.cs
+++ LAMA/MainPage.xaml.cs
@@ -1,25 +1,58 @@
-﻿namespace LAMA
+﻿using LAMA.Core;
+using LAMA.Auth;
+using System.Collections.ObjectModel;
+
+namespace LAMA
 {
     public partial class MainPage : ContentPage
     {
-
-        public double AppIconSize { get; set; }
+        public ObservableCollection<string> Categories { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+
+            Categories = new ObservableCollection<string>
+            {
+                "General Health",
+                "Mental Health",
+                "Sexual Health",
+                "Chronic Conditions & Autoimmune",
+                "Medication & Drug Interactions",
+                "Alternative & Holistic Medicine"
+            };
+            BindingContext = this;
         }
 
-        private async void OnSignUpTapped(object sender, EventArgs e)
+        private async void OnProfileClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//SignUpPage");  // Navigate to SignUpPage
+            await Shell.Current.GoToAsync(nameof(ProfilePage));
         }
 
-        private async void OnSignInTapped(object sender, EventArgs e)
+        private async void OnMedFactsClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//ProfilePage");  // Navigate to SignUpPage
+            await Shell.Current.GoToAsync(nameof(MedFactsPage));
         }
 
+        private async void OnCategoryClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(CategoryPage));
+        }
+
+        private async void OnMessagesClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(MessagesPage));
+        }
+
+        private async void OnSettingsClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(SettingsPage));
+        }
+
+        private async void OnMPSignupClicked(object sender, EventArgs e) 
+        {
+            await Shell.Current.GoToAsync(nameof(SignUpPage));
+        }
     }
 
 }
