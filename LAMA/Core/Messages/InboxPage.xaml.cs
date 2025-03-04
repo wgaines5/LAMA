d1b4e692ff3a3197a1b6663c94785c694aef3eb0
David James Street <62122942+dneutron0@users.noreply.github.com>
2025-03-03 23:27:44 -0600

    Refactor messaging system to use InboxPage
    Updated AppShell.xaml to replace Messages section with Inbox.
    Introduced InboxPage and ChatViewModel for managing chat
    messages. Removed old MessagesPage and updated project
    references accordingly.
    
    Refactor messaging feature to use InboxPage
    
    Updated AppShell.xaml to replace Messages section with Inbox.
    Introduced InboxPage.xaml and InboxPage.xaml.cs for new chat
    functionality, including a ChatViewModel for message handling.
    Removed MessagesPage.xaml.cs and updated project file to
    reflect these changes.

--- LAMA/Core/Messages/InboxPage.xaml.cs
+++ LAMA/Core/Messages/InboxPage.xaml.cs
@@ -1,4 +1,5 @@
-
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace LAMA.Core.Messages;
 
@@ -6,11 +7,59 @@ public partial class InboxPage : ContentPage
 {
 	public InboxPage()
 	{
-        InitializeComponent();
+		InitializeComponent();
+		BindingContext = new ChatViewModel();
 	}
 
-    private void InitializeComponent()
-    {
-        throw new NotImplementedException();
-    }
+	private void OnSendMessage(object sender, EventArgs e)
+	{
+		if (BindingContext is ChatViewModel chatViewModel)
+		{
+			chatViewModel.SendMessageCommand.Execute(this);
+		}
+	}
+}
+
+public class ChatViewModel : BindableObject
+{
+	private string _newMessage;
+	public ObservableCollection<ChatMessage> Messages { get; set; } = new ObservableCollection<ChatMessage>();
+	public string NewMessage
+	{
+		get => _newMessage;
+
+		set
+		{
+			if (_newMessage != value)
+			{
+				_newMessage = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	public ICommand SendMessageCommand { get; }
+
+	public ChatViewModel()
+	{
+		SendMessageCommand = new Command(SendMessage);
+	}
+
+	private void SendMessage()
+	{
+		if (!string.IsNullOrWhiteSpace(NewMessage))
+		{
+			Messages.Add(new ChatMessage { Content = NewMessage, IsUserMessage = true});
+			NewMessage = string.Empty;
+			OnPropertyChanged(nameof(NewMessage));
+
+			// Simulate response
+			Messages.Add(new ChatMessage { Content = "Thanks for your message.", IsUserMessage = false});
+		}
+	}
+}
+
+public class ChatMessage
+{
+	public string Content { get; set; }
+	public bool IsUserMessage { get; set; }
 }
