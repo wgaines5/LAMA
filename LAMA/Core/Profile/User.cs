78edc83624a8a1e04e737d284718902c560c51ab
David James Street <62122942+dneutron0@users.noreply.github.com>
2025-03-10 18:54:47 -0500

    ProfilePage UI updates

---
+++ LAMA/Core/Profile/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMA.Core.Profile
+{
+    class User
+    {
+        string userID = "";
+
+        string firstName = "";
+
+        string lastName = "";
+
+        string profilePictureUrl = "";
+
+        int queries = 0;
+
+    }
+}
--- LAMA/Core/ProfilePage.xaml.cs
+++ LAMA/Core/ProfilePage.xaml.cs
@@ -2,9 +2,6 @@ namespace LAMA.Core;
 
 public partial class ProfilePage : ContentPage
 {
-
-	private string fullName;
-
 	public ProfilePage()
 	{
 		InitializeComponent();
