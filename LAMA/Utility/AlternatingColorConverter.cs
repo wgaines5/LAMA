8e6e46d368d6ef6359d1ab14b0624357d6b8cb84
mhaire <MPHaire@student.fullsail.edu>
2025-03-12 10:29:50 -0500

    Added a background imnage to the medical facts page. Added a card layout to the medical facts page. Added a Alternating color converter to control the color of the cards on medical facts page.

---
+++ LAMA/Utility/AlternatingColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMA
+{
+    public class AlternatingColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int index)
+            {
+                return index % 2 == 0 ? Colors.SteelBlue : Colors.DarkGray;
+            }
+            return Colors.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
