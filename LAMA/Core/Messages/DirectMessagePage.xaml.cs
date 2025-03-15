f269414f0bf9fb44355ca940c8193cec510c8dee
David James Street <62122942+dneutron0@users.noreply.github.com>
2025-03-14 19:06:52 -0500

    Refactor messaging interface and add chat functionality
    Updated `AppShell.xaml` to switch to `DirectMessagePage`.
    Introduced `ChatViewModel` for managing chat messages and added
    support for sending messages. Removed chat handling from
    `InboxPage.xaml.cs`. Added `ChatMessage` class to represent
    individual messages. Updated project file to ensure proper
    compilation of new and existing files.

--- LAMA/Core/Messages/DirectMessagePage.xaml.cs
+++ LAMA/Core/Messages/DirectMessagePage.xaml.cs
@@ -8,8 +8,71 @@ namespace LAMA.Core.Messages;
 
 public partial class DirectMessagePage : ContentPage
 {
-    public DirectMessagePage()
-    {
-        InitializeComponent();
-    }
+	public DirectMessagePage()
+	{
+		InitializeComponent();
+		BindingContext = new ChatViewModel(new MessageService("https://localhost:5001/chathub"));
+	}
+
+	private void OnSendMessage(object sender, EventArgs e)
+	{
+		if (BindingContext is ChatViewModel chatViewModel)
+		{
+			chatViewModel.SendMessageCommand.Execute(this);
+		}
+	}
 }
+
+public class ChatViewModel : BindableObject
+{
+	private MessageService _messageService;
+	private string _newMessage;
+	public ObservableCollection<ChatMessage> Messages { get; set; } = new ObservableCollection<ChatMessage>();
+	public string NewMessage
+	{
+		get => _newMessage;
+
+		set
+		{
+			if (_newMessage != value)
+			{
+				_newMessage = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	public ICommand SendMessageCommand { get; }
+
+	public ChatViewModel(MessageService messageService)
+	{
+		_messageService = messageService;
+		SendMessageCommand = new Command(async () => await SendMessage());
+
+		_messageService.MessageReceived += (message) =>
+		{
+			Messages.Add(new ChatMessage { Content = message, IsUserMessage = false });
+		};
+
+		Task.Run(async () => await _messageService.ConnectAsync());
+	}
+
+	private async Task SendMessage()
+	{
+		if (!string.IsNullOrWhiteSpace(NewMessage))
+		{
+			await _messageService.SendMessageAsync(NewMessage);
+			Messages.Add(new ChatMessage { Content = NewMessage, IsUserMessage = true});
+			NewMessage = string.Empty;
+			OnPropertyChanged(nameof(NewMessage));
+		}
+	}
+}
+
+public class ChatMessage
+{
+	public string Content { get; set; }
+	public bool IsUserMessage { get; set; }
+
+}
+
+
