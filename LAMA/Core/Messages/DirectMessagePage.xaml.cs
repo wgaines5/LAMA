0a9b8b70e27e37410918c6c04fc0401d7d739ca9
David James Street <62122942+dneutron0@users.noreply.github.com>
2025-03-14 19:01:35 -0500

    File structure updates
    Refactored `DirectMessagePage` to `InboxPage

--- LAMA/Core/Messages/DirectMessagePage.xaml.cs
+++ LAMA/Core/Messages/DirectMessagePage.xaml.cs
@@ -1,15 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using LAMA.Core.Messages;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace LAMA.Core.Messages;
 
-public partial class DirectMessagePage : ContentPage
+public partial class InboxPage : ContentPage
 {
-    public DirectMessagePage()
-    {
-        InitializeComponent();
-    }
+	public InboxPage()
+	{
+		InitializeComponent();
+		BindingContext = new ChatViewModel(new MessageService("https://localhost:5001/chathub"));
+	}
+
+	private void OnSendMessage(object sender, EventArgs e)
+	{
+		if (BindingContext is ChatViewModel chatViewModel)
+		{
+			chatViewModel.SendMessageCommand.Execute(this);
+		}
+	}
 }
+
+public class ChatViewModel : BindableObject
+{
+	private MessageService _messageService;
+	private string _newMessage;
+	public ObservableCollection<ChatMessage> Messages { get; set; } = new ObservableCollection<ChatMessage>();
+	public string NewMessage
+	{
+		get => _newMessage;
+
+		set
+		{
+			if (_newMessage != value)
+			{
+				_newMessage = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+	public ICommand SendMessageCommand { get; }
+
+	public ChatViewModel(MessageService messageService)
+	{
+		_messageService = messageService;
+		SendMessageCommand = new Command(async () => await SendMessage());
+
+		_messageService.MessageReceived += (message) =>
+		{
+			Messages.Add(new ChatMessage { Content = message, IsUserMessage = false });
+		};
+
+		Task.Run(async () => await _messageService.ConnectAsync());
+	}
+
+	private async Task SendMessage()
+	{
+		if (!string.IsNullOrWhiteSpace(NewMessage))
+		{
+			await _messageService.SendMessageAsync(NewMessage);
+			Messages.Add(new ChatMessage { Content = NewMessage, IsUserMessage = true});
+			NewMessage = string.Empty;
+			OnPropertyChanged(nameof(NewMessage));
+		}
+	}
+}
+
+public class ChatMessage
+{
+	public string Content { get; set; }
+	public bool IsUserMessage { get; set; }
+
+}
+
+
--- LAMA/Core/Messages/InboxPage.xaml.cs
+++ LAMA/Core/Messages/InboxPage.xaml.cs
@@ -1,77 +1,15 @@
-using System.Collections.ObjectModel;
-using System.Windows.Input;
-using LAMA.Core.Messages;
-using Microsoft.AspNetCore.SignalR.Client;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LAMA.Core.Messages;
 
-public partial class InboxPage : ContentPage
+public partial class DirectMessagePage : ContentPage
 {
-	public InboxPage()
-	{
-		InitializeComponent();
-		BindingContext = new ChatViewModel(new MessageService("https://localhost:5001/chathub"));
-	}
-
-	private void OnSendMessage(object sender, EventArgs e)
-	{
-		if (BindingContext is ChatViewModel chatViewModel)
-		{
-			chatViewModel.SendMessageCommand.Execute(this);
-		}
-	}
+    public DirectMessagePage()
+    {
+        InitializeComponent();
+    }
 }
-
-public class ChatViewModel : BindableObject
-{
-	private MessageService _messageService;
-	private string _newMessage;
-	public ObservableCollection<ChatMessage> Messages { get; set; } = new ObservableCollection<ChatMessage>();
-	public string NewMessage
-	{
-		get => _newMessage;
-
-		set
-		{
-			if (_newMessage != value)
-			{
-				_newMessage = value;
-				OnPropertyChanged();
-			}
-		}
-	}
-	public ICommand SendMessageCommand { get; }
-
-	public ChatViewModel(MessageService messageService)
-	{
-		_messageService = messageService;
-		SendMessageCommand = new Command(async () => await SendMessage());
-
-		_messageService.MessageReceived += (message) =>
-		{
-			Messages.Add(new ChatMessage { Content = message, IsUserMessage = false });
-		};
-
-		Task.Run(async () => await _messageService.ConnectAsync());
-	}
-
-	private async Task SendMessage()
-	{
-		if (!string.IsNullOrWhiteSpace(NewMessage))
-		{
-			await _messageService.SendMessageAsync(NewMessage);
-			Messages.Add(new ChatMessage { Content = NewMessage, IsUserMessage = true});
-			NewMessage = string.Empty;
-			OnPropertyChanged(nameof(NewMessage));
-		}
-	}
-}
-
-public class ChatMessage
-{
-	public string Content { get; set; }
-	public bool IsUserMessage { get; set; }
-
-}
-
-
