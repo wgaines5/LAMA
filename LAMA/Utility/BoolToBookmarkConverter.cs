89b2988eded70b525372a20ca0cf460da7f45f04
mhaire <MPHaire@student.fullsail.edu>
2025-03-14 20:26:35 -0500

    Added a checkbox to be able to save specific facts to a user profile. Needs backend functionality still. Changed the text color and background color of the send button in the messages screen. Changed the text color and the background color of the input bar for the messages screen.

--- LAMA/Utility/BoolToBookmarkConverter.cs
+++ LAMA/Utility/BoolToBookmarkConverter.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Globalization;
 
 namespace LAMA
 {
-    public class BoolToColorConverter : IValueConverter
+    public class BoolToBookmarkConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool)value ? "#26c7b6" : "#01a4ff";
+            if (value is bool isBookmarked)
+            {
+                return isBookmarked ? "\u2605" : "\u2606";
+                // return isBookmarked ? "Saved" : "Save";
+            }
+            return "\u2606";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
