dcd77a844b6d50df4552191453a02c69640bd24c
mhaire <MPHaire@student.fullsail.edu>
2025-03-23 02:12:30 -0500

    Added basic sample page to show proof of concept for communicating with the Firestore database. Added Sample.xaml and Sample.xaml.cs and added the navigation to the sample test pages to the appshell.

---
+++ LAMA/Services/FirestoreServices.cs
@@ -0,0 +1,85 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMA.Services
+{
+    public class FirestoreServices
+    {
+        private FirestoreDb db;
+
+        private async Task SetupFirestore()
+        {
+            if (db == null)
+            {
+                var stream = await FileSystem.OpenAppPackageFileAsync("admin-sdk.json");
+                var reader = new StreamReader(stream);
+                var contents = reader.ReadToEnd();
+
+                db = new FirestoreDbBuilder
+                {
+                    ProjectId = "lama-7054a",
+
+                    ConverterRegistry = new ConverterRegistry
+                    {
+                        new DateTimeToTimeStampConverter()
+                    },
+                    JsonCredentials = contents
+                }.Build();
+            }
+        }
+
+        public async Task InsertSampleModel(SampleModel sample)
+        {
+            await SetupFirestore();
+            await db.Collection("SampleModels").AddAsync(sample);
+        }
+
+        public async Task<List<SampleModel>> GetSampleModel()
+        {
+            await SetupFirestore();
+            var data = await db
+                .Collection("SampleModels")
+                .GetSnapshotAsync();
+
+            var sampleModels = data.Documents
+                .Select(doc =>
+                {
+                    var sampleModel = doc.ConvertTo<SampleModel>();
+                    sampleModel.Id = doc.Id;
+                    return sampleModel;
+                }).ToList();
+            return sampleModels;
+        }
+    }
+    [FirestoreData]
+    public class SampleModel
+    {
+        [FirestoreProperty]
+        public string Id { get; set; }
+        [FirestoreProperty]
+        public string Name { get; set; }
+        [FirestoreProperty]
+        public string Description { get; set; }
+        [FirestoreProperty]
+        public DateTime Created { get; set; }
+    }
+
+    public class DateTimeToTimeStampConverter : IFirestoreConverter<DateTime>
+    {
+        public object ToFirestore(DateTime value) => Timestamp.FromDateTime(value.ToUniversalTime());
+
+        public DateTime FromFirestore(object value)
+        {
+            if (value is Timestamp timestamp)
+            {
+                return timestamp.ToDateTime();
+            }
+            throw new ArgumentException("Invalid value");
+        }
+    }
+}
