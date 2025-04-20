266084c8e67852fb40910000be874197c0996ca3
wgaines5 <williegaines5@gmail.com>
2025-04-20 18:29:39 -0400

    SignUpPage coverted  to Firestore and working

--- LAMA/Auth/SignUpPage.xaml.cs
+++ LAMA/Auth/SignUpPage.xaml.cs
@@ -37,16 +37,11 @@ public partial class SignUpPage : ContentPage
     {
         try
         {
-            UserCredential mpCredential = await _authClient.CreateUserWithEmailAndPasswordAsync(
-                _email.Text, _password.Text);
-
+            UserCredential mpCredential = await _authClient.CreateUserWithEmailAndPasswordAsync(_email.Text, _password.Text);
             string uid = mpCredential.User.Uid;
-
-            // ID token for authentication
             string idToken = await mpCredential.User.GetIdTokenAsync();
 
-            // Usser fields for profile - Database
-            object userData = new
+            object profile = new
             {
                 email = _email.Text,
                 username = _username.Text,
@@ -60,29 +55,59 @@ public partial class SignUpPage : ContentPage
                 createdAt = DateTime.UtcNow.ToString("o")
             };
 
-            string json = JsonSerializer.Serialize(userData);
+            string json = ConvertToFirestoreJson(profile);
+            string url = $"https://firestore.googleapis.com/v1/projects/lama-60ddc/databases/(default)/documents/medical_providers?documentId={uid}&access_token={idToken}";
 
-            //  Firebase RT Database url 
-            string url = $"https://lama-60ddc-default-rtdb.firebaseio.com/medical_providers/{uid}.json?auth={idToken}";
+            using HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
 
-            
-            HttpClient client = new HttpClient();
-            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            HttpResponseMessage response = await client.PutAsync(url, content);
+            HttpResponseMessage response = await client.SendAsync(request);
             if (!response.IsSuccessStatusCode)
             {
-                string errorBody = await response.Content.ReadAsStringAsync();
-                throw new Exception($"Firebase Error: {response.StatusCode}\n\n{errorBody}");
+                string error = await response.Content.ReadAsStringAsync();
+                throw new Exception($"Firestore Error: {response.StatusCode}\n\n{error}");
             }
 
-            //  Success msg shown to proceed 
             await DisplayAlert("Success", "Your Account Has Been created, Now Awaiting verification.", "OK");
             await Shell.Current.GoToAsync($"//{nameof(MPDashBoard)}");
         }
-        catch 
+        catch (Exception ex)
         {
-            await DisplayAlert("Error","Something Crashed Try Again", "OK");
+            await DisplayAlert("Error", $"Something Crashed\n{ex.Message}", "OK");
         }
+    }
+
+    private string ConvertToFirestoreJson(object data)
+    {
+        JsonElement element = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(data));
+        Dictionary<string, object> fields = new();
+
+        foreach (JsonProperty property in element.EnumerateObject())
+        {
+            object value;
+            switch (property.Value.ValueKind)
+            {
+                case JsonValueKind.String:
+                    value = new { stringValue = property.Value.GetString() };
+                    break;
+                case JsonValueKind.Number:
+                    value = new { integerValue = property.Value.GetInt32().ToString() };
+                    break;
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                    value = new { booleanValue = property.Value.GetBoolean() };
+                    break;
+                default:
+                    value = new { stringValue = property.Value.ToString() };
+                    break;
+            }
+
+            fields[property.Name] = value;
+        }
+
+        return JsonSerializer.Serialize(new { fields });
     }
 }
