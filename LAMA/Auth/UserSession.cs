b5e8708a340b503d5e1d0bc036fe80f7167c4eb6
wgaines5 <williegaines5@gmail.com>
2025-05-06 22:55:58 -0400

    Sign In Page Created and functional

--- LAMA/Auth/UserSession.cs
+++ LAMA/Auth/UserSession.cs
@@ -1,4 +1,6 @@
-  using Firebase.Auth;
+using Firebase.Auth;
+
+using System.Text.Json;
 
 namespace LAMA.Auth
 {
@@ -7,6 +9,7 @@ namespace LAMA.Auth
         public static UserCredential Credential { get; set; }
 
         public static LAMA.Core.Profile.User CurrentUser { get; set; }
+        public static JsonElement CurrentMedicalProfessional { get; set; }
         public static string Token { get; set; }
         public static string ProviderId { get; set; }
         public static string SessionId {  get; set; }
