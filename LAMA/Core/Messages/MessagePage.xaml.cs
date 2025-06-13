bafd4a05637a758c1031728001c6a2486ef6d9f0
wgaines5 <williegaines5@gmail.com>
2025-06-13 18:28:34 -0400

    Modified Message update

--- LAMA/Core/Messages/MessagePage.xaml.cs
+++ LAMA/Core/Messages/MessagePage.xaml.cs
@@ -276,7 +276,7 @@ public partial class MessagePage : ContentPage
                 }
             }
 
-            await Task.Delay(1000);
+            await Task.Delay(1000, token);
         }
     }
 
